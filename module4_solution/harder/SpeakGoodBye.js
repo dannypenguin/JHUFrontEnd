b0007cf4cd23efdf2e7ef69779abf3e4fa1a02f9
Danny Peng <DannyPengXD@Gmail.com>
2022-09-24 22:33:43 -0400

    "finished assignment 4"

---
+++ module4_solution/harder/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function(window) {
+    var speakWord = "Good Bye";
+    var byeSpeaker = {};
+    byeSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    }
+    byeSpeaker.speakSimple = function(name) {
+        return speakWord + " " + name;
+    }
+    window.byeSpeaker = byeSpeaker;
+})(window);
